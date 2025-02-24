001e215cb2206cd1f8dc984262bf61bd538ae1e3
wling-art <wlingzhenyu@163.com>
2025-02-25 01:52:13 +0800

    🐛 验证用户是否存在

--- app/services/user/crud.py
+++ app/services/user/crud.py
@@ -2,15 +2,31 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
 from app.services.auth.auth import verify_token
+from app.services.user.models import User
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme)) -> dict:
     payload = await verify_token(token)
+
     if payload is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    if "sub" not in payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    user = await User.get_or_none(username=payload["sub"])
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
